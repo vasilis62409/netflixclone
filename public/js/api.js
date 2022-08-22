8fe9d31fdfa10a4c8bc52c567a60fa16a33ec9c5
vasilis62409 <52891304+vasilis62409@users.noreply.github.com>
2022-08-22 21:24:58 +0300

    Add files via upload

---
+++ public/js/api.js
@@ -0,0 +1,8 @@
+let api_key="d3b0dee306031bdd960359de1b799e30";
+
+let img_url = "https://image.tmdb.org/t/p/w500";
+let genres_list_http = "https://api.themoviedb.org/3/genre/movie/list?";
+let movie_genres_http = "https://api.themoviedb.org/3/discover/movie?";
+
+let original_img_url = "https://image.tmdb.org/t/p/original";
+let movie_detail_http = "https://api.themoviedb.org/3/movie";
